02c52412e304a3f87e5d37ebb56e7112aedf7e2a
obahchimaobi <anthonyobah37@gmail.com>
2024-08-18 08:09:46 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SmartHR</title>
+
+    {{-- favicon --}}
+    <link rel="shortcut icon" href="{{ asset('icons/smart-hr.png') }}" type="image/x-icon">
 </head>
 <body>
     
